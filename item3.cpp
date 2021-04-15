8010a59b5ae0366b86f42fdd5a580671b38b7bb8
gustavohimeno <58175587+gustavohimeno@users.noreply.github.com>
2021-04-15 00:46:55 -0300

    tratamento para condições específicas

--- item3.cpp
+++ item3.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 /* função que retorna o mínimo de operações necessários para transformar
 uma string em outra removendo e inserindo caracteres no seu fim */
-int min_ops(string prim, string seg)
+int min_ops(string first, string sec, int i, int j, int &count)
     {
     int min_ops = 0;
-    int i = prim.size()-1;
-    int j = seg.size()-1;
-    //
-    for(int count = 0; count < i; count++)
+    
+    for(count = 0; count < i; count++)
         {
-        if(prim[count] != seg[count])
+        if(first[count] != sec[count])
             {
             min_ops = i + j - 2*count;
             return min_ops;
@@ -27,15 +25,37 @@ int min_ops(string prim, string seg)
 outra dado um número k máximo de operações */
 bool ConcatRemove(string s, string t, int k)
     {
-    int ops = min_ops(s,t);
-    if(ops <= k)
+    int i = s.size()-1;
+    int j = t.size()-1;
+    
+    //when k bigger than (i+j) I can remove all characters and concat all the new
+    if(k > i+j)
         {
         return true;
         }
-    else
+    
+    //when k smaller than (i+j) i have to analyse the minimum operations necessary to convert
+    int count = 0;
+    int ops = min_ops(s, t, i, j, count);
+    if(ops == k)
+        {
+        return true;
+        }
+    if(ops > k)
         {
         return false;
         }
+    
+    //when ops < k, I have 2 alternatives: 1- concat and remove, 2- remove when the string is empty
+    if((k-ops)%2 == 0)
+        {
+        return true;
+        }
+    if(count == 0)
+        {
+        return true;
+        }
+    return false;
     }
 
 
@@ -55,7 +75,7 @@ int main()
     fgets(t, 100, stdin);
 
     //solicita que o usuário insira o máximo de operações
-    puts("Type the number of operations (max = 100): ");    
+    puts("Type the number of operations (min = 0 and max = 100): ");    
     //recebe o máximo de operações
     cin >> k;
     
@@ -68,4 +88,7 @@ int main()
         {
         printf("no");
         }
+    
+    return 0;
     }
+
